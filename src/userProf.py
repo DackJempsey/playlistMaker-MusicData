50e905104d92c8b975e00fea48902215fef3df39
DackJempsey <jade8185@colorado.edu>
2018-11-28 18:48:16 -0700

    made src, and userProf, renamed src.py to main.py

---
+++ src/userProf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  userProf.py
+#  
+#  Copyright 2018 Jack Dempsey <jackdempsey@rgnt2-102-74-dhcp.int.colorado.edu>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  This portion of the code creates a user profile based on saved songs
+#	things to do: take random sample of tracks from user.
+#	Get the type of song, and can also get related artists.
+
+
+def userProf(sp, user):
+	
+
+
+
